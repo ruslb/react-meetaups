73ced00dbaeec5e0ab2765da51f1fd09df373921
Ruslan Barna <ruslbarna@gmail.com>
2022-12-16 04:22:11 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import Todo from "./components/Todo";
+import { Routes ,Route } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
 
-
-function App()  {     
-return ( 
-          <div>
-           <h1>My Todos</h1>
-			  <Todo text='Start page' />
-			  <Todo text='Dashboard' />
-			  <Todo text='Second Dashboard' />
-			
-       </div>
-);
+function App() {
+  return (
+    <div>
+		<Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
